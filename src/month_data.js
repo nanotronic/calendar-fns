2af9ba2e35d12ab26d13f13fdc55e086e2c88a33
nanotronic <nanotron@drehmoment.org>
2017-07-27 17:04:04 +0200

    Update month_data.js

--- src/month_data.js
+++ src/month_data.js
@@ -1,12 +1,15 @@
 var startOfWeek = require('date-fns/src/start_of_week');
 var startOfMonth = require('date-fns/src/start_of_month');
 var endOfWeek = require('date-fns/src/end_of_week');
+var endOfMonth = require('date-fns/src/end_of_month');
+var endOfWeek = require('date-fns/src/end_of_week');
 var addDays = require('date-fns/src/add_days');
 var eachDay = require('date-fns/src/each_day');
 var weekData = require('./week_data');
 var isSameMonth = require('date-fns/src/is_same_month');
+var isWithinRange = require('date-fns/is_within_range')
 
-var NUMBER_OF_WEEKS = 6;
+var MAX_NUMBER_OF_WEEKS = 6;
 
 /**
  * @param {string|date} dirtyDate
@@ -16,11 +19,14 @@ var NUMBER_OF_WEEKS = 6;
 var monthData = function(dirtyDate, weekStartsAt) {
   var date = new Date(dirtyDate);
   var startDate = startOfWeek(startOfMonth(date), weekStartsAt);
+  var endDate = endOfWeek(endOfMonth(date), weekStartsAt);
   var month = [];
-  var weekIndex, week, startOfWeekDate;
+  var weekIndex, week, startOfWeekDate, endOfWeekDate;
 
-  for (weekIndex = 0; weekIndex < NUMBER_OF_WEEKS; weekIndex++) {
+  for (weekIndex = 0; weekIndex < MAX_NUMBER_OF_WEEKS; weekIndex++) {
     startOfWeekDate = addDays(startDate, weekIndex * 7);
+    endOfWeekDate = addDays(startOfWeekDate, 6);
+    if (!isWithinRange(endDate, startOfWeekDate, endOfWeekDate)
     week = weekData(startOfWeekDate, weekStartsAt).map(
       function(dayData) {
         dayData.isDummy = !isSameMonth(date, dayData.date);
