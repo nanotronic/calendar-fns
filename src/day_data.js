8a446b99c50dd798cdd3a0c69e9a1f8835dc830d
nanotronic <nanotron@drehmoment.org>
2017-07-28 11:28:23 +0200

    Update day_data.js

--- src/day_data.js
+++ src/day_data.js
@@ -1,9 +1,9 @@
-var startOfDay = require('date-fns/src/start_of_day');
-var isToday = require('date-fns/src/is_today');
-var isWeekend = require('date-fns/src/is_weekend');
-var isFirstDayOfMonth = require('date-fns/src/is_first_day_of_month');
-var isLastDayOfMonth = require('date-fns/src/is_last_day_of_month');
-var isFuture = require('date-fns/src/is_future');
+var startOfDay = require('date-fns/start_of_day');
+var isToday = require('date-fns/is_today');
+var isWeekend = require('date-fns/is_weekend');
+var isFirstDayOfMonth = require('date-fns/is_first_day_of_month');
+var isLastDayOfMonth = require('date-fns/is_last_day_of_month');
+var isFuture = require('date-fns/is_future');
 
 /**
  * @param {date|string} dirtyDate
